0ac0830275e86e2436df8a84bea240c6b77e8733
Sota Saito <sotasaito@macbookairshiroringo.local>
2018-12-19 19:13:03 +0900

    Initial Commit

---
+++ hyoi2Tests/hyoi2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  hyoi2Tests.swift
+//  hyoi2Tests
+//
+//  Created by Sota Saito on 2018/12/19.
+//  Copyright © 2018 unwithdrawal. All rights reserved.
+//
+
+import XCTest
+@testable import hyoi2
+
+class hyoi2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
